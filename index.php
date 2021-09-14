dec37deb8858ad0d4574d266728192c8f474657b
karenvalencia-bit <karenochoa196@gmail.com>
2021-09-14 15:39:14 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body class="bg-dark">
+    <header>
+
+    </header>
+
+    <main>
+        <div class="container-fluid">
+            <div class="row">
+                 <div class="col-8 g-3 align-self-center ">
+                     <form action="">
+                        <h1 class="align-text-center">JUEGOS DIVERTIDOS PARA APRENDER</h1>
+
+                        <div class="input-group mt-5">
+
+                             <input name="numero1" id="numero1" type="number" class="form-control border border-5 border-warning" >
+                             
+
+                             <input name="numero2" id="numero2" type="number" class="form-control ms-3 border border-5 border-info ">
+                     
+                             <input name="numero3" id="numero3" type="number" required class="form-control ms-3 border border-5 border-danger">
+                   
+
+                        </div>
+
+                        <button type="button" class="btn btn-outline-success w-100 mt-5" ><i class="fas fa-calculator"></i>  CALCULAR   <i class="fas fa-calculator"></i> </button>
+
+                     </form>
+
+                 </div>
+                 m
+
+                <div class="col-4 align-self-center mt-5">
+                    <img src="https://firebasestorage.googleapis.com/v0/b/juego-cpc.appspot.com/o/foto1.jpg?alt=media&token=4ce23bfe-2b47-4552-a02b-556a9930d94d" alt="img1" class="img-fluid w-100 h-200  ">
+                </div>
+            
+        </div>
+
+    </main>
+
+    <footer>
+
+    </footer>
+    
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/7b642ec699.js" crossorigin="anonymous"></script>
+</body>
+</html>
