669d8527c03fc28bc07022837c9183429f46dd0d
karen <70981253+karenvalencia-bit@users.noreply.github.com>
2021-09-15 16:51:54 -0500

    miercoles presencial

--- controlador.js
+++ controlador.js
@@ -1,6 +1,11 @@
 let calcular=document.getElementById("calcular");
+let siguiente=document.getElementById("siguiente");
+
+
+
 
 calcular.addEventListener("click", verificarRespuesta);
+siguiente.addEventListener("click", pasarpregunta);
 
 function verificarRespuesta(){
     
@@ -8,8 +13,8 @@ function verificarRespuesta(){
     let numero2=document.getElementById("numero2").value;
     let resultado=document.getElementById("resultado").value;
     let gif=document.getElementById("gif");
-    let gif2=document.getElementById("gif2");
     let texto=document.getElementById("texto");
+    //
 
     
 
@@ -17,31 +22,23 @@ function verificarRespuesta(){
 
     if(resultado==calcularResultado){
         console.log("tu respuesta esta correcta");
-
-        gif.classList.remove("visible");
-        gif.classList.add("invisible");
-        
-        gif2.parentNode.removeChild(gif2);
        
         texto.textContent="tu respuesta esta correcta";
 
         gif.classList.remove("invisible");
         gif.classList.add("visble");
+        gif.src="https://giphy.com/embed/xT9DPiHFM8Iy3hiC3e";
 
 
     }
     else{
-        console.log("ojoo")
-        
-        gif.classList.remove("visible");
-        gif.classList.add("invisible");
-        
-        gif.parentNode.removeChild(gif);
+        console.log("tu respuesta esta incorrecta");
+       
+        texto.textContent="tu respuesta esta incorrecta";
 
-        texto.textContent="ojo esta incorrecto";
-
-        gif2.classList.remove("invisible");
-        gif2.classList.add("visible");
+        gif.classList.remove("invisible");
+        gif.classList.add("visble");
+        gif.src="https://giphy.com/embed/JT7Td5xRqkvHQvTdEu";
 
         
 
@@ -51,5 +48,41 @@ function verificarRespuesta(){
     
 
 }
+function pasarpregunta() {
+
+    let resultado=document.getElementById("resultado").value;
+
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+      }
+
+
+    if(resultado==""){
+        texto.textContent="ojo no contestaste";
+
+        gif.classList.remove("invisible");
+        gif.classList.add("visble");
+        gif.src="https://giphy.com/embed/oZvduOsUmlsk";
+
+    }
+
+    else{
+        function getRandomInt(min, max) {
+            return Math.floor(Math.random() * (max - min)) + min;
+          }
     
+        let resultado=document.getElementById("resultado");
+          resultado.value="";
+    
+        let numero1=document.getElementById("numero1");
+        numero1.value=getRandomInt(1,10);
+    
+        let numero2=document.getElementById("numero2");
+        numero2.value=getRandomInt(1,10);
+
+    }
+    
+    
+   
+  }
 
